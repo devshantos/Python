faf8d8c79a388be7c0ef23ee40a404b13cffec65
Saikatul Islam <75540959+devshantos@users.noreply.github.com>
2022-11-12 09:59:23 +0600

    Create variable concept.py

---
+++ variable concept.py
@@ -0,0 +1,8 @@
+#Let's diclaration variable name which you want
+greetings="Hi,"
+First_name="Siaktul Islam"
+Last_name="Shanto"
+
+# Now print this variable
+print(greetings, First_name, Last_name)
+#copy the code and run your pycharm or vs code
