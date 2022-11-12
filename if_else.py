ab66e9a6043b96bae2784ef00e13632e7e9fbb2f
Saikatul Islam <75540959+devshantos@users.noreply.github.com>
2022-11-12 22:43:21 +0600

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,16 @@
+
+num=int(input("Enter your choice number: "))
+if num>=33:
+    print('Congratulations, You are passed in the subject')
+else:
+    print('Sorry, You are the not promoted.', 'Next time good luck')
+
+
+#Calculate different between two nunumbers
+num1=int(input("Enter your number: "))
+num2=int(input("Enter your second number: "))
+
+if num1>num2:
+    print('Your biggest number is: ', num1)
+else:
+    print('Your smallest number is: ', num2)
