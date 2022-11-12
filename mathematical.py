7f8ba85a2b1bafda2db0a07ef26449a25455e69f
Saikatul Islam <75540959+devshantos@users.noreply.github.com>
2022-11-12 20:53:55 +0600

    Create mathematical.py

---
+++ mathematical.py
@@ -0,0 +1,11 @@
+egg_price="Ege per piece price is: 10tk"
+print(egg_price)
+egg_quantity= int(input("What amount do you want?: ----- "))
+total= 10 * egg_quantity
+print("Take your invoice, It's amount of ",total, "tk")
+
+#Dollar count
+print('Dollar exchange rate by BDT:- 102 tk')
+bdt_tk=int(input("Put your dollar here:-"))
+dollar_rate= 102.5 * bdt_tk
+print('Your BDT tk is:', dollar_rate)
