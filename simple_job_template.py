0e4a5e9deeda88d77ffc0737160831db25f54f40
Saikatul Islam <75540959+devshantos@users.noreply.github.com>
2022-11-14 12:07:44 +0600

    Create simple_job_template.py

---
+++ simple_job_template.py
@@ -0,0 +1,7 @@
+country_name=input("Enter your country name: ")
+country_code= input("Enter country code: ")
+your_number= input("Enter your number: ")
+
+setup= f"The {country_name} country code {country_code} will allow you to call {country_name} from another country. {country_name} telephone code {country_code} is dialed after the\nIDD. {country_name} international dialing {country_code} is followed by an area code. Your number {country_code}{your_number} saved us our databased"
+
+print(setup)
