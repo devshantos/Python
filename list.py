29bbff404c8f14cbff2e120afdc8e8e5c96546f3
Saikatul Islam <75540959+devshantos@users.noreply.github.com>
2022-11-20 09:40:21 +0600

    Create list.py

---
+++ list.py
@@ -0,0 +1,21 @@
+movies = []
+movies.append('jakc soli')
+movies.append('Smith')
+movies.append('Rose')
+movies.append('John Doe')
+movies.insert(2,'Scarly')
+print(movies)
+
+
+actress= ['Angeli', 'Dengeli', 'Monjeli']
+actress_time =['Doli', 'Ploy', 'Done']
+actress.extend(actress_time)
+print(actress)
+
+
+'''number= ('I', 'Love', 'You')
+sentence = ' '.join(number)
+print(sentence)'''
+Word= [10,20,45,78,56,34,435,546,34,43546,4565,556,556,55,]
+Word.sort(reverse=True)
+print(Word)
